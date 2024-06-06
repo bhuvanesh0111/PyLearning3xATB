1e0dbd322f91972e1fffb746499d7685ed52dd10
bhuvanesh011 <bhuvabhuvi03@gmail.com>
2024-06-06 21:37:08 +0530

    This is Lab2

---
+++ Ex_05062024/Lab002.py
@@ -0,0 +1,29 @@
+#print("This is Bhuvanesh" , "Baskaran" , sep=",")
+#print("I am 24 years","old",sep="-")
+
+print("I wanted to learn", "Automation Testing", sep="-", end="\n\n")
+print("I started learning", "Python", sep="-")
+
+print(max(12,-22))
+print(max(1,2,3,4,5,67,8,9,-0.1))
+
+a="Bhuvanesh"
+b=25
+c=True
+print(type(a))
+print(type(b))
+print(type(c))
+
+Fav_number=25
+print(type(Fav_number))
+Fav_number="Twenty one"
+print(type(Fav_number))
+
+A=12
+B=1
+C=10
+print(A+B+C)
+
+Primary_Skills = input("Enter your Primary skills : ")
+Secondary_Skills = input("Enter your Secondary skills : ")
+print("Primary Skills are",Primary_Skills,"and" , Pu"Secondary Skills are",Secondary_Skills)
